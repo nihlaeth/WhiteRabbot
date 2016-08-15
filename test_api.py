3f322e957d0648994902c182b20ec8a5dd3fa7e2
nihlaeth <info@nihlaeth.nl>
2016-08-15 07:44:30 +0200

    tests for _validate_*

--- test_api.py
+++ test_api.py
@@ -133,3 +133,32 @@ class Test_get_schedule():
         with DummyDB() as session:
             result = api.get_schedule(session, 1)
             assert_equals(result.success, False)
+
+
+# pylint: disable=protected-access
+class Test_validate_ordering():
+
+    def test_valid_data(self):
+        result = api._validate_ordering(1)
+        assert_equals(result.success, True)
+        assert_equals(result.value, 1)
+
+    def test_non_int(self):
+        result = api._validate_ordering("not an int")
+        assert_equals(result.success, False)
+
+
+class Test_validate_shift_name():
+
+    def test_valid_data(self):
+        result = api._validate_shift_name("test")
+        assert_equals(result.success, True)
+        assert_equals(result.value, "test")
+
+    def test_non_str(self):
+        result = api._validate_shift_name(0)
+        assert_equals(result.success, False)
+
+    def test_empty_str(self):
+        result = api._validate_shift_name("")
+        assert_equals(result.success, False)
