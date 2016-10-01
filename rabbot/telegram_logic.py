3f0e3d42fa011a75430f27c30d3388247f2caa16
Sietse Brouwer <sbbrouwer@gmail.com>
2016-10-02 00:40:01 +0200

    some user stories for the telegram logic

---
+++ rabbot/telegram_logic.py
@@ -0,0 +1,15 @@
+# Jim types @rabbot June 15
+# Rabbot gives Jim a list of shifts on that day.
+# Jim clicks one
+# Jim posts "Jim is looking for someone to covers shift X (via @rabbot)"
+
+# Carla type @rabbot open
+# Rabbot gives her a list of open shifts
+# Carla clicks one
+# Carla posts "Carla covers shift X (via @rabbot)"
+
+# Blaise type @rabbot mutations
+# Rabbot gives zir a list of open and altered shifts
+
+# What if I want to tentatively open a shift? The scenario
+# "If anybody can cover this, great; otherwise, I'll do it myself."
