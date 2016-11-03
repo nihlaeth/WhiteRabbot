7ab45590b3098a53bdb845c44ff846a8d5c98a97
nihlaeth <info@nihlaeth.nl>
2016-11-03 12:23:43 +0100

    use exceptions instead of Return class

--- rabbot/api/helpers.py
+++ rabbot/api/helpers.py
@@ -1,41 +1,16 @@
-class Result:
-    """Represent a result that can be either a value or an error"""
-    def __init__(self,
-                 message: str="",
-                 success: bool=True,
-                 value=None,
-                 errors: list=None
-                ):
-        self.success = success
-        self.message = message
-        self.value = value
-        if errors is None:
-            self.errors = list()
-        else:
-            self.errors = errors
+class InvalidInput(Exception):
+
+    """Unusable input."""
+
+def validate_ordering(ordering) -> None:
+    """Raise InvalidInput if ordering is not valid."""
+    if not isinstance(ordering, int):
+        raise InvalidInput('Ordering should be an integer, and not {}')
 
 
-def validate_ordering(us_ordering) -> Result:
-    """Return Result with validated ordering, or with errors"""
-    if not isinstance(us_ordering, int):
-        return Result(
-            success=False,
-            errors=['Ordering should be an integer, and not {}'
-                    .format(us_ordering)])
-    else:
-        return Result(value=us_ordering)
-
-
-def validate_shift_name(us_name: str) -> Result:
-    """Return Result with validated shift name, or with errors"""
-    result = Result()
-    if not isinstance(us_name, str):
-        result.success = False
-        result.errors.append('Shift name must be a string')
-    elif len(us_name) < 1:
-        result.success = False
-        result.errors.append('Shift name may not be empty')
-    else:
-        result.value = us_name
-    return result
-
+def validate_shift_name(name: str) -> None:
+    """Raise InvalidInput if name is not valid."""
+    if not isinstance(name, str):
+        raise InvalidInput('Shift name must be a string')
+    elif len(name) < 1:
+        raise InvalidInput('Shift name may not be empty')
